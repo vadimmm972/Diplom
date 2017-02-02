7a08675910db2b812745ad2486f26924104fe4f0
Vadym Kobylynskiy <vadimmm972@gmail.com>
2017-02-02 23:57:53 +0200

    Diplom(0.8)

---
+++ Sell_Buy/Controllers/CreateMagazineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sell_Buy.Controllers
+{
+    public class CreateMagazineController : Controller
+    {
+        // GET: CreateMagazine
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
